74e8d7178601b212b6f7f58e516fe6e3c2e879df
JonatanCespedes <retratos.jonatan@gmail.com>
2023-05-17 19:21:13 -0300

    Agrega service orderItem

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,14 +1,65 @@
 const { Order } = require("../database/models");
 
 const getOrders = async () => {
-    return await Order.findAll();
+  return await Order.findAll();
 };
 
 const getOrderById = async (id) => {
-    return await Order.findByPk(id);
+  return await Order.findByPk(id);
+};
+
+/* getOrdersByUser */
+const getOrderByUser = (userId) => {
+  return Order.findOne({
+    where: {
+      userId,
+    },
+    include: [
+      { association: "orderItems", include: [{ association: "products" }] },
+    ],
+  });
+};
+
+const insertOrder = async (data) => {
+    try {
+        return await Order.create(data);
+    } catch (error) {
+        console.error("Error while fetching order :", error);
+        throw new Error("Error fetching order ");
+    }
+};
+
+const updateOrder = async (data, Id) => {
+    try {
+        return await Order.create(data, {
+            where: {
+                id: Id
+            }
+        });
+    } catch (error) {
+        console.error("Error while fetching order :", error);
+        throw new Error("Error fetching order ");
+    }
+};
+
+const deleteOrder = async (id) => {
+    try {
+        return await Order.destroy({
+            where: {
+                id
+            }
+        });
+    } catch (error) {
+        console.error("Error while fetching order :", error);
+        throw new Error("Error fetching order ");
+    }
 };
 
 module.exports = {
-    getOrders, 
-    getOrderById
+  getOrders,
+  getOrderById,
+  getOrderByUser,
+  insertOrder,
+  updateOrder,
+  deleteOrder
 };
---
+++ src/services/orderItems.service.js
@@ -0,0 +1,77 @@
+const { OrderItem } = require("../database/models");
+
+const getOrdersItems = async () => {
+  try {
+    return await OrderItem.findAll();
+  } catch (error) {
+    console.error("Error while fetching order item:", error);
+    throw new Error("Error fetching order item");
+  }
+};
+
+const getOrderItemById = async (id) => {
+  try {
+    return await OrderItem.findByPk(id);
+  } catch (error) {
+    console.error("Error while fetching order item:", error);
+    throw new Error("Error fetching order item");
+  }
+};
+
+const getOrderItemsByOrder = async (orderId) => {
+  try {
+    return await OrderItem.findOne({
+      where: {
+        orderId,
+      },
+      include: [{ association: "products" }],
+    });
+  } catch (error) {
+    console.error("Error while fetching order item:", error);
+    throw new Error("Error fetching order item");
+  }
+};
+
+const insertOrderItem = async (data) => {
+    try {
+        return await OrderItem.create(data);
+    } catch (error) {
+        console.error("Error while fetching order item:", error);
+        throw new Error("Error fetching order item");
+    }
+};
+
+const updateOrderItem = async (data, itemId) => {
+    try {
+        return await OrderItem.create(data, {
+            where: {
+                id: itemId
+            }
+        });
+    } catch (error) {
+        console.error("Error while fetching order item:", error);
+        throw new Error("Error fetching order item");
+    }
+};
+
+const deleteOrderItem = async (id) => {
+    try {
+        return await OrderItem.destroy({
+            where: {
+                id
+            }
+        });
+    } catch (error) {
+        console.error("Error while fetching order item:", error);
+        throw new Error("Error fetching order item");
+    }
+};
+
+module.exports = {
+  getOrdersItems,
+  getOrderItemById,
+  getOrderItemsByOrder,
+  insertOrderItem,
+  updateOrderItem,
+  deleteOrderItem
+};
